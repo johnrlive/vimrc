354ce51607958a64d75c2dd7c5a63c35a3c81764
Johnrlive <[omitted]>
2018-10-03 12:36:46 -0400

    added option to uncomment for FizzyFinder & RipGrep

--- vim_install_ubuntu.sh
+++ vim_install_ubuntu.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-echo 'Ubuntu Install VIM'
+echo 'Step 1: Install VIM'
 sudo apt-get install vim
 
-echo 'Ubuntu Install Fuzzy Finder'
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+### UNCOMMENT these 3 lines to install Fuzzy Finder ###
+#echo 'Ubuntu Install Fuzzy Finder'
+#git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+#~/.fzf/install
+
+### UNCOMMENT these 2 lines to install RipGrep ###
+#echo 'Ubuntu install ripgrep'
+#sudo apt-get install ripgrep
 
 echo 'Step 2: Setup VIM'
 sh vim_setup.sh
